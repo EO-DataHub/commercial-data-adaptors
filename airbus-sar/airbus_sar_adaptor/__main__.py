4123e05b1f86d145142af8e88467cdc05525dc85
Alex Palmer <alex.palmer@telespazio.com>
2024-11-28 10:53:07 +0000

    Import ordering

--- airbus-sar/airbus_sar_adaptor/__main__.py
+++ airbus-sar/airbus_sar_adaptor/__main__.py
@@ -4,8 +4,6 @@ import mimetypes
 import sys
 from enum import Enum
 
-from pulsar import Client as PulsarClient
-
 from airbus_sar_adaptor.api_utils import is_order_in_progress, post_submit_order
 from airbus_sar_adaptor.s3_utils import (
     list_objects_in_folder,
@@ -19,6 +17,7 @@ from airbus_sar_adaptor.stac_utils import (
     update_stac_order_status,
     write_stac_item_and_catalog,
 )
+from pulsar import Client as PulsarClient
 
 logging.basicConfig(
     level=logging.INFO,
